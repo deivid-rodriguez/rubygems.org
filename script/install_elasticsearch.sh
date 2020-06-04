6f6c74dbc83cf61efb76846b3ea6cd0df209e1aa
David Rodríguez <deivid.rodriguez@riseup.net>
2020-06-04 13:26:43 +0200

    Try fix elasticsearch install

---
+++ script/install_elasticsearch.sh
@@ -0,0 +1,7 @@
+set -e
+
+curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.8.10.deb
+
+sudo dpkg -i --force-confnew elasticsearch-6.8.10.deb
+
+sudo service elasticsearch restart
