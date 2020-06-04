94fc18b0fbcab9facb4085f0a64f67965ea7e777
David Rodríguez <deivid.rodriguez@riseup.net>
2020-06-04 16:58:55 +0200

    More debug

--- script/install_elasticsearch.sh
+++ script/install_elasticsearch.sh
@@ -2,6 +2,6 @@ set -e
 
 curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.8.10.deb
 
-sudo dpkg -i --force-confnew elasticsearch-6.8.10.deb
+sudo dpkg -i --force-confold elasticsearch-6.8.10.deb
 
 sudo service elasticsearch start
